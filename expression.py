8c28279fcfdb994a0bc3ae64b45fae210614d110
ngarokoe <74895258+ngarokoe@users.noreply.github.com>
2020-11-23 15:45:23 +0500

    Update and rename workitem.py to expression.py

---
+++ expression.py
@@ -0,0 +1,13 @@
+import random
+
+
+class WorkItem():
+    '''
+    
+    '''
+
+    # Конструктор
+    def __init__(self):
+        self.A = random.randint(1, 500)
+        self.B = random.randint(1, 500)
+        
