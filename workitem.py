5db3b42f22a8949fc5db616a873d7ae08f19f538
ngarokoe <74895258+ngarokoe@users.noreply.github.com>
2020-11-23 15:59:27 +0500

    Update workitem.py

--- workitem.py
+++ workitem.py
@@ -3,11 +3,19 @@ import random
 
 class WorkItem():
     '''
-    
+    Рабочий элемент - "Решение системы"
+    A1x + B1y = C1
+    A2x + B2y = C2
     '''
 
     # Конструктор
     def __init__(self):
-        self.A = random.randint(1, 500)
-        self.B = random.randint(1, 500)
-        
+        self.A1 = random.randint(1, 100)
+        self.B1 = random.randint(1, 100)
+        self.C1 = random.randint(1, 100)
+        self.A2 = random.randint(1, 100)
+        self.B2 = random.randint(1, 100)
+        self.C2 = random.randint(1, 100)
+
+    x = 0
+    y = 0
